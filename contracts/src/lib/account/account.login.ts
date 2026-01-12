1a8ef11a33fc02bfde30d826361794c1cb3700d9
internethero00 <egormix1703@gmail.com>
2026-01-12 23:14:14 +0200

    + validation

--- contracts/src/lib/account/account.login.ts
+++ contracts/src/lib/account/account.login.ts
@@ -1,7 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export namespace AccountLogin {
   export const topic = 'account.login.command';
   export class Request {
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
     email!: string;
+    @IsString({ message: 'Password is required' })
+    @IsNotEmpty({ message: 'Password is required' })
     password!: string;
   }
   export class Response {
--- contracts/src/lib/account/account.register.ts
+++ contracts/src/lib/account/account.register.ts
@@ -1,11 +1,19 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export namespace AccountRegister {
   export const topic = 'account.register.command';
   export class Request {
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
     email!: string;
+    @IsString({ message: 'Password is required' })
+    @IsNotEmpty({ message: 'Password is required' })
     password!: string;
+    @IsString({ message: 'Display name is required' })
+    @IsOptional()
     displayName?: string;
   }
   export class Response {
-    email!: string
+    email!: string;
   }
 }
