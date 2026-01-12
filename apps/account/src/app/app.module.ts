20dbef6b4a458de9f5ab7fd2ca5342e532af0d8e
internethero00 <egormix1703@gmail.com>
2026-01-12 23:14:39 +0200

    + auth rmq

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoConfig } from './configs/mongo.config';
+import { RMQModule } from 'nestjs-rmq';
+import { getRMQConfig } from './configs/rmq.config';
 
 @Module({
   imports: [
@@ -12,6 +14,7 @@ import { getMongoConfig } from './configs/mongo.config';
       envFilePath: 'envs/.account.env',
     }),
     MongooseModule.forRootAsync(getMongoConfig()),
+    RMQModule.forRootAsync(getRMQConfig()),
     UserModule,
     AuthModule
   ],
--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { RegisterDto } from './auth.controller';
 import { UserRepository } from '../user/repositories/user.repository';
 import { UserEntity } from '../user/entities/user.entity';
 import { UserRole } from '@school/interfaces';
 import { JwtService } from '@nestjs/jwt';
 import { Types } from 'mongoose';
+import { AccountRegister } from '@school/contracts';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +13,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async register({ email, password, displayName }: RegisterDto) {
+  async register({ email, password, displayName }: AccountRegister.Request) {
     const oldUser = await this.userRepository.findUser(email);
     if (oldUser) {
       throw new Error('User already exists');
