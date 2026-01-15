f416da3be4fffc00b0c7ac375c2f3dd51e8c9703
internethero00 <egormix1703@gmail.com>
2026-01-15 22:45:31 +0200

    + account contracts

---
+++ contracts/src/lib/account/account.user-info.ts
@@ -0,0 +1,13 @@
+import { IsString } from 'class-validator';
+import { IUser } from '@school/interfaces';
+
+export namespace AccountUserInfo {
+  export const topic = 'account.user-info.query';
+  export class Request {
+    @IsString()
+    id!: string;
+  }
+  export class Response {
+    user!: Omit<IUser, 'passwordHash'>;
+  }
+}
