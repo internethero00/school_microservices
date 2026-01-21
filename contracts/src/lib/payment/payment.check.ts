bbbee60256cbb89b58ce2e62ce5a1a322dec8a26
internethero00 <egormix1703@gmail.com>
2026-01-21 18:24:00 +0200

    + payment.check contract

---
+++ contracts/src/lib/payment/payment.check.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export namespace PaymentCheck{
+  export const topic = 'payment.check.query';
+  export class Request {
+    @IsString({ message: 'courseId is required' })
+    @IsNotEmpty({ message: 'courseId is required' })
+    courseId!: string;
+
+    @IsString({ message: 'userId is required' })
+    @IsNotEmpty({ message: 'userId is required' })
+    userId!: string;
+
+  }
+  export class Response {
+    status!: 'cancelled' | 'success' | 'progress';
+  }
+}
